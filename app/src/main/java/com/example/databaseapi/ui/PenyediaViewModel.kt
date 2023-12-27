750c5cdd12a8c3ce7fe3fabf3f0bfb7d8d701727
Dimas Damar Buana <dimasluxmundi@gmail.com>
2023-12-27 21:17:49 +0700

    added InsertViewModel into PenyediaViewModel

--- app/src/main/java/com/example/databaseapi/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/databaseapi/ui/PenyediaViewModel.kt
@@ -8,12 +8,17 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.databaseapi.KontakApplication
 import com.example.databaseapi.ui.home.viewmodel.HomeViewModel
+import com.example.databaseapi.ui.kontak.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     val Factory = viewModelFactory {
         initializer {
             HomeViewModel(aplikasiKontak().container.kontakRepositori)
+        }
+
+        initializer {
+            InsertViewModel(aplikasiKontak().container.kontakRepositori)
         }
     }
 }
