dc35007529c96e913dd55f91274d4cbc637bb9d5
Dimas Damar Buana <dimasluxmundi@gmail.com>
2023-12-27 17:06:27 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/databaseapi/ui/PenyediaViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.databaseapi.ui
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.databaseapi.KontakApplication
+import com.example.databaseapi.ui.home.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiKontak().container.kontakRepositori)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiKontak(): KontakApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KontakApplication)
