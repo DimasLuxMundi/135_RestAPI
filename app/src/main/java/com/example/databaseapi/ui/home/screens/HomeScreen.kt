759b7607680612860fa815e16a8daaf6ec69d4df
Dimas Damar Buana <dimasluxmundi@gmail.com>
2023-12-27 20:16:34 +0700

    modified homescreen function into homestatus

--- app/src/main/java/com/example/databaseapi/ui/home/screens/HomeScreen.kt
+++ app/src/main/java/com/example/databaseapi/ui/home/screens/HomeScreen.kt
@@ -34,25 +34,31 @@ import com.example.databaseapi.R
 import com.example.databaseapi.model.Kontak
 import com.example.databaseapi.ui.home.viewmodel.KontakUIState
 
-//@Composable
-//fun HomeScreen(
-//    kontakUIState: KontakUIState,
-//    retryAction: () -> Unit,
-//    modifier: Modifier = Modifier
-//) {
-//    when (kontakUIState) {
-//        is KontakUIState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
-//        is KontakUIState.Success -> KontakLayout(
-//            kontak = kontakUIState.kontak,
-//            modifier = modifier.fillMaxWidth(),
-//
-//
-//        )
-//
-//        is KontakUIState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
-//    }
-//
-//}
+@Composable
+fun HomeStatus(
+    kontakUIState: KontakUIState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Kontak) -> Unit = {},
+    onDetailClick: (Int) -> Unit
+) {
+    when (kontakUIState) {
+        is KontakUIState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is KontakUIState.Success -> KontakLayout(
+            kontak = kontakUIState.kontak,
+            modifier = modifier.fillMaxWidth(),
+            onDetailClick = {
+                onDetailClick(it.id)
+            },
+            onDeleteClick = {
+                onDeleteClick(it)
+            }
+        )
+
+        is KontakUIState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+
+}
 
 /*
 The Home screen displaying the loading message
